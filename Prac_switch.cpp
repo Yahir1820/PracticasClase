5303130af18fdc7ee5ca4139dd355270e29bc5a1
TEJEDA JIMENEZ ALAN YAHIR <zS25018113@estudiantes.uv.mx>
2025-10-06 09:44:30 -0600

    Se agrega segundo programa

--- Prac_switch.cpp
+++ Prac_switch.cpp
@@ -9,22 +9,24 @@ int main () {
    cout << "1) Torta" << endl;
    cout << "2) Palomitas" << endl;
    cout << "3) Agua" << endl;
-   cout << "4) Salir" << endl;
    cout << "Selecciona el numero del producto a elegir: ";
    cin >> comida;
 
-if (comida == 1) {
-    cout << "La torta vale $35" << endl;
-} else if (comida == 2) {
+switch (comida) {
+    case 1:
+        cout << "La torta vale $35" << endl;
+        break;
+    case 2:
         cout << "Las palomitas valen $15" << endl;
-} else if (comida == 3) {
-    cout << "El agua vale $20" << endl;
-} else if (comida == 4) {
-    cout << "Saliendo del programa";
-} else {
-    cout << "Ingrese una opcion valida";
+        break;
+    case 3:
+        cout << "El agua vale $20" << endl;
+        break;
+    default:
+        cout << "Ingrese una opcion valida";
+        break;
 }
-   } while (comida != 4);
+} while (comida != 4);
    
     return 0;
 }
